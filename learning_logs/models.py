e3b24450e6375b607e009c751c4e7bc99a89b080
ty9065 <949137774@qq.com>
2017-10-25 17:34:32 +0800

    基本功能完成

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
+	user = models.ForeignKey(User)
 	text = models.CharField(max_length=200)
 	date_added = models.DateTimeField(auto_now_add=True)
 
---
+++ users/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request,authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
