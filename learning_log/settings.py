13c8f54636539d9beb888997a4a9dd621dadbbd6
ty9065 <949137774@qq.com>
2017-10-27 11:57:42 +0800

    设置STATIC_ROOT

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -147,4 +147,7 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
